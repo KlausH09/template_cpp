b194f7afb8df3452738804d6dc159c6f9f8a0c16
Klaus <k.happacher@gmail.com>
2021-04-08 11:34:51 +0200

    [FIX] add catch2 as submodule

--- cmake/KHTesting.cmake
+++ cmake/KHTesting.cmake
@@ -5,11 +5,9 @@ option(MemCheck "enable checking for memory leaks" OFF)
 option(CodeCov "enable code coverage" OFF)
 
 if(Build_Tests)
-  find_package(Catch2 REQUIRED)
   include(CTest)
-  include(Catch)
+  enable_testing()
 endif(Build_Tests)
-
 
 
 # linting
@@ -58,15 +56,9 @@ if(CodeCov)
   endif()
 endif()
 
-# main test file (comiled only once)
-set(mainKHTest mainKHTest)
-add_library(${mainKHTest} STATIC ${CMAKE_CURRENT_LIST_DIR}/mainKHTest.cpp)
-target_link_libraries(${mainKHTest} PRIVATE Catch2::Catch2)
-set_target_properties(${mainKHTest} PROPERTIES CXX_CLANG_TIDY "")
-
 # add tests
 function(add_khTest TEST_EXE)
-  target_link_libraries(${TEST_EXE} PRIVATE Catch2::Catch2 ${mainKHTest})
+  target_link_libraries(${TEST_EXE} PRIVATE Catch2::Catch2WithMain)
   set_target_properties(${TEST_EXE} PROPERTIES CXX_CLANG_TIDY "")
-  catch_discover_tests(${TEST_EXE})
+  add_test(${TEST_EXE} ${TEST_EXE})
 endfunction()
